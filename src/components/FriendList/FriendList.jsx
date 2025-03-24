2c41b0ba90d504c898f8fc909dd4e60b4bf7d7e9
Volodymyr-Stupnyk <vstupnik44@gmail.com>
2025-03-24 09:28:38 +0200

    hw r 1

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,15 @@
+const FriendList = () => {
+    return (
+        <ul>
+            <li>
+                <div>
+                  <img src="" alt="Avatar" width="48" />
+                  <p>Friend name</p>
+                  <p>Friend status</p>
+                </div>
+            </li>
+        </ul>
+    );
+};
+
+export default FriendList;
